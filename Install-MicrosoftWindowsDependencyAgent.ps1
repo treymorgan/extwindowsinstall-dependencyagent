b68f3ba809d0450789c3b20e4aff05698f31824b
Trey Morgan <trey@info-technology.com>
2017-05-22 13:28:04 -0500

    Update Install-MicrosoftWindowsDependencyAgent.ps1

--- Install-MicrosoftWindowsDependencyAgent.ps1
+++ Install-MicrosoftWindowsDependencyAgent.ps1
@@ -1,2 +1,7 @@
 mkdir c:\Temp2\dependencyagentwindows -Force
 Invoke-WebRequest -Uri "https://aka.ms/dependencyagentwindows" -OutFile "c:\Temp2\dependencyagentwindows\InstallDependencyAgent-Windows.exe" 
+
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/treymorgan/extwindowsinstall-dependencyagent/master/Install-MicrosoftWindowsDependencyAgent.ps1" -OutFile "c:\Temp2\dependencyagentwindows\Install-MicrosoftWindowsDependencyAgent.ps1" 
+
+cd c:\Temp2\dependencyagentwindows
+./InstallDependencyAgent-Windows.exe /S
