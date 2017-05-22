e82bca7108347633754bb3a682373d95e382576d
Trey Morgan <trey@info-technology.com>
2017-05-22 14:11:52 -0500

    Update Install-MicrosoftWindowsDependencyAgent.ps1

--- Install-MicrosoftWindowsDependencyAgent.ps1
+++ Install-MicrosoftWindowsDependencyAgent.ps1
@@ -1,7 +1,30 @@
-mkdir c:\Temp2\dependencyagentwindows -Force
-Invoke-WebRequest -Uri "https://aka.ms/dependencyagentwindows" -OutFile "c:\Temp2\dependencyagentwindows\InstallDependencyAgent-Windows.exe" 
+#requires -version 3
+<#
+.SYNOPSIS
+  The following script will install the Microsoft Windows Dependency Agent 
 
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/treymorgan/extwindowsinstall-dependencyagent/master/Install-MicrosoftWindowsDependencyAgent.ps1" -OutFile "c:\Temp2\dependencyagentwindows\Install-MicrosoftWindowsDependencyAgent.ps1" 
+.DESCRIPTION
+  <Brief description of script>
 
-cd c:\Temp2\dependencyagentwindows
+#>
+
+$TempDirectory = "C:\Temp2\DependencyAgentWindows\"
+$MSDependencyAgentMediaURL = "https://aka.ms/dependencyagentwindows"
+$MediaFileName = $TempDirectory + "InstallDependencyAgent-Windows.exe"
+$URLofPSInstallScript = "https://raw.githubusercontent.com/treymorgan/extwindowsinstall-dependencyagent/master/Install-MicrosoftWindowsDependencyAgent.ps1"
+$PSInstallScriptName = $TempDirectory + "Install-MicrosoftWindowsDependencyAgent.ps1"
+
+#Create temporary directory to store the script and installation media
+New-Item $TempDirectory -type directory -Force
+
+#Download the Microsoft Dependency Agent Media and store it in the temporary location specified above
+Invoke-WebRequest -Uri $MSDependencyAgentMediaURL  -OutFile $MediaFileName 
+
+#Download the PowerShell script that installs the agent
+Invoke-WebRequest -Uri $URLofPSInstallScript -OutFile $PSInstallScriptName
+
+#Swith directories to the location where the PowerShell script and installation media are stored
+Set-Location -Path $TempDirectory
+
+#Peform a Silent installation of the Microsoft Dependency Agent
 ./InstallDependencyAgent-Windows.exe /S
