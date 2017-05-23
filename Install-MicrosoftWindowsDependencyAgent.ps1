7f116540d7e873de2dd6f3a79c97472cc1ff179a
Trey Morgan <trey@info-technology.com>
2017-05-22 19:51:37 -0500

    Update Install-MicrosoftWindowsDependencyAgent.ps1

--- Install-MicrosoftWindowsDependencyAgent.ps1
+++ Install-MicrosoftWindowsDependencyAgent.ps1
@@ -9,87 +9,55 @@
 .LOCATION
 The following script is located here: https://raw.githubusercontent.com/treymorgan/extwindowsinstall-dependencyagent/master/Install-MicrosoftWindowsDependencyAgent.ps1 
 
-
 #>
 
-
-#Obtains the environment for Public Azure and add it to a variable
-$env = Get-AzureRMEnvironment -Name AzureCloud
-
-#Login to Azure
-Add-AzureRMAccount -EnvironmentName $env
+#Creates New Event Log Source if it does not already exist
+New-EventLog -LogName Application -Source "MSDependencyAgent" -ErrorAction SilentlyContinue
 
 
-cls
-#Declare Variables for the script
-#$VMName = "awesomevmone"
-#$RGName = "deletemeresourcegp"
-$ExtensionName
-$Location = "East US"
+#Script Variables
+$TempDirectory = "C:\Temp\DependencyAgentWindows\"
+$MSDependencyAgentMediaURL = "https://aka.ms/dependencyagentwindows"
+$MediaFileName = $TempDirectory + "InstallDependencyAgent-Windows.exe"
 $URLofPSInstallScript = "https://raw.githubusercontent.com/treymorgan/extwindowsinstall-dependencyagent/master/Install-MicrosoftWindowsDependencyAgent.ps1"
-$PSInstallScriptName = "Install-MicrosoftWindowsDependencyAgent.ps1"
-$ExtensionName = 'DependencyAgentWindows'
+$PSInstallScriptName = $TempDirectory + "Install-MicrosoftWindowsDependencyAgent.ps1"
 
-#Get list of servers and group names from a csv file
-$serversrgrouplist = Import-Csv -LiteralPath C:\temp\ServerList.csv
+#Create temporary directory to store the script and installation media
+New-Item $TempDirectory -type directory -Force
 
-#Function to install the Dependency agent using a list of servers and resource groups in a csv file
-Function Install-DependencyAgent {
+#Download the Microsoft Dependency Agent Media and store it in the temporary location specified above
+Invoke-WebRequest -Uri $MSDependencyAgentMediaURL  -OutFile $MediaFileName 
 
-#Loop through the list of servers and groups and install the oms agent
-foreach ($serverrgroup in $serversrgrouplist)  {
+#Download the PowerShell script that installs the agent
+Invoke-WebRequest -Uri $URLofPSInstallScript -OutFile $PSInstallScriptName
 
-#convert the server name and group name into a variable
-$server = $serverrgroup.Name
-$resourcegroup = $serverrgroup.rgroup
+#Swith directories to the location where the PowerShell script and installation media are stored
+Set-Location -Path $TempDirectory
 
-Try
-{
-#Deploys the ARM Custom Script Extension to install the Microsoft Dependency Agent for Windows
-Set-AzureRmVMCustomScriptExtension -ResourceGroupName $RGName `
-    -VMName $VMName `
-    -Location $Location `
-    -FileUri $URLofPSInstallScript `
-    -Run $PSInstallScriptName `
-    -Name $ExtensionName
- 
-}
-Catch
-{
- Throw $_
- exit 1
+#Peform a Silent installation of the Microsoft Dependency Agent
+./InstallDependencyAgent-Windows.exe /S
+
+#Check Microsoft Dependency Agent Status
+$MicrosoftDependencyAgentServiceStatus = Test-Path -Path "C:\Program Files\Microsoft Dependency Agent\bin\MicrosoftDependencyAgent.exe"
+
+#sleep for 1 min
+Start-Sleep -Seconds 60
+
+#Log an event in the windows application event log indicating the success of failure of the agent installation (this can be picked up by OMS)
+If ($MicrosoftDependencyAgentServiceStatus.count -gt 0) {
+$MicrosoftDependencyAgentServiceStatusStatus = $MicrosoftDependencyAgentServiceStatus.Status
+$Message =  "The Microsoft Dependency Agent is installed" | Out-String
+
+Write-EventLog -LogName Application -Source "MSDependencyAgent" -EntryType Information -EventID 1 -Message $Message 
 }
 
-#capture extension execution results
-$Installresults =  ((Get-AzureRmVM -Name $VMName -ResourceGroupName $RGName -Status).Extensions | Where-Object {$_.Name -eq $ExtensionName}).Substatuses
-$Desplaystatus = $Installresults.DisplayStatus
-$Message = $Installresults.message
+Else {
 
+$Message =  "The Microsoft Dependency Agent was not detected or an error occurred while attempting to detect the agent service" | Out-String
 
-         #custom object
-         $data = @{
-         'Server' = $server
-         'RGroup' = $resourcegroup
-         'DisplayStatus' = $Desplaystatus
-         'Message' = $Message
-                 }
-
-
-           #Output the results
-            New-Object -TypeName psobject -Property $data
-
-}
+Write-EventLog -LogName Application -Source "MSDependencyAgent" -EntryType Warning -EventID 2 -Message $Message 
 }
 
-#Add function results to a variable
-$results = Install-DependencyAgent
-
-#export the results to a csv file
-$results | Export-Csv -Path C:\Temp\finalresult.csv -Force -NoTypeInformation
-<#
-#Removes the ARM Custom Script Extension that installs the Microsoft Dependency Agent for Windows
-
-    remove-AzureRmVMCustomScriptExtension -ResourceGroupName deletemeresourcegp `
-    -VMName 'awesomevmone' `
-    -Name DependencyAgentWindows
-#>
+#remove the install media
+$FilePath = $TempDirectory + "InstallDependencyAgent-Windows.exe"
+Remove-Item $FilePath -Force
